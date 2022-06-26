88ce9dc0ae60ba96a69c3c75f2856d693368624e
smile98173 <smile98173@gmail.com>
2022-06-26 18:20:40 +0800

    second
    修改圖片路徑問題

--- js/index.js
+++ js/index.js
@@ -2,13 +2,13 @@ $('#mode').on('click', function (e) {
      var butclass = $(this).attr("class");
      if (butclass == "btn btn-light"){
       
-      document.body.setAttribute('style', 'background-image: url("../img/hand-dark.jpg")');
+      document.body.setAttribute('style', 'background-image: url("img/hand-dark.jpg")');
       $(this).attr('class', 'btn btn-dark');
       $(this).html('dark');
      
      } else {
      
-     document.body.setAttribute('style', 'background-image: url("../img/hand.jpg")');
+     document.body.setAttribute('style', 'background-image: url("img/hand.jpg")');
      $(this).attr('class', 'btn btn-light');
      $(this).html('light');
      }
